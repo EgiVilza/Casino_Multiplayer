e896fcab57bf9b1c16277abd33c3ad411e04afdc
EgiVilza <egivilza@gmail.com>
2021-04-14 18:08:40 -0500

    Added components

--- client/src/pages/SignupPage/index.js
+++ client/src/pages/SignupPage/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "./style.css"
 
-function Login() {
+function Signup() {
     return (
         <div>
-
+            
         </div>
     )
 }
 
-export default Login
+export default Signup
