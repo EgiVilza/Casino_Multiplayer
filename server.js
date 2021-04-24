d60c90518ab81792d988a7a8c88f2402713bb151
cawleygl <grantlcawley@gmail.com>
2021-04-24 11:29:57 -0400

    Fixed server.js to reflect file rename

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session")
-const router = require("./routes/api/playersAPI")
+const router = require("./routes/api/players")
 const cors = require("cors")
 const middleware = require("./config/middleware/jsonWebToken")
 
